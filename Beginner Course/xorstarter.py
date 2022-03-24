fa5c99034db02b6c26b4492e5be386a81712aad0
mutiararflnsh <rifliansahmutiara@gmail.com>
2022-03-24 17:09:33 +0700

    tugas 3

--- Beginner Course/xorstarter.py
+++
@@ -1,7 +0,0 @@
-text = "label"
-flag_value = ""
-
-for i in text:
-    flag_value += chr(ord(i)^13)
-
-print(flag_value)
---
+++ Modular Arithmetic Course/modularArith2.py
@@ -0,0 +1,7 @@
+from math import gcd
+
+a = 273246787654
+p = 65537
+
+if gcd(a,p)==1: #a and p are coprime
+        print(1)
